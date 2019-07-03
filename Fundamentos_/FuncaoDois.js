3db7d4c5083a1ec13e82a58eabe9e1098bab96e4
748413 <Jessica.Colina@Cognizant.com>
2019-07-03 10:35:14 -0300

    Novos exercícios sobre Função e ArrowFunction

---
+++ Fundamentos_/FuncaoDois.js
@@ -0,0 +1,21 @@
+//Armazenando uma função em uma variável:
+//Usando uma function anônima, isto é, ela não tem nome!
+const imprimirSoma = function(a,b){
+    console.log(a + b)
+}
+
+imprimirSoma(2, 3)
+
+//Armazenando uma função Arrow em uma variável: 
+//A seta é uma forma reduzida de falar função: 
+const soma = (a, b) => {
+    return a + b; 
+}
+console.log(soma(2,3))
+
+//retorno implicito
+//Atenção, se não tenho chaves em uma função, isso significa que terá apenas uma única linha! Como exemplo abaixo. Que é outra forma de escrever uma função. Se eu tivesse apenas 1 parâmetro, não haveria necessidade de colocar os parênteses.              
+
+                //arrow function
+const subtracao = (a,b) => a - b
+console.log(subtracao(2,3))
---
+++ Fundamentos_/funcaoEmQuaseTudo.js
@@ -0,0 +1,4 @@
+console.log(typeof(Object))
+//função é basilar no JS
+class Product {}
+console.log(Product)
