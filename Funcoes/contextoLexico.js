b3bd88793469f7d509a9dd79e3ac2a2900bca29d
748413 <Jessica.Colina@Cognizant.com>
2019-07-17 20:27:00 -0300

    Contexto léxico

--- Funcoes/contextoLexico.js
+++ Funcoes/contextoLexico.js
@@ -9,4 +9,4 @@ function exec(){
     minhaFuncao()
 }
 
-exec()//ele retorna o valor 'global', mesmo tendo declarado uma constante mais próxima dessa função, pois a função minhaFuncao é maior, e ela carrega consigo um contexto
+exec()//ele retorna o valor 'global', mesmo tendo declarado uma constante mais próxima dessa função, pois a função minhaFuncao é maior, e ela carrega consigo um contexto. Ou seja, ela não vai procurar no local de execução, mas sim no local que foi criada, por isso ela pega a constante no escopo maior. 
