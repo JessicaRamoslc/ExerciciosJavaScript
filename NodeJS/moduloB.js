4fecffcbf6afa654c4938233f3bcf9b87eddfea7
748413 <Jessica.Colina@Cognizant.com>
2019-08-06 16:25:11 -0300

    Como exportar módulos em NodeJS

--- NodeJS/moduloB.js
+++ NodeJS/moduloB.js
@@ -1,7 +1,7 @@
 // Forma de exportar mais comum: 
 module.exports = {
-    bomDia: 'Bom dia', 
-    boaNoite(){
+    bomDia: 'Bom dia', //posso colocar atributo
+    boaNoite(){ //posso colocar função
         return 'Boa Noite'
     }
 }
