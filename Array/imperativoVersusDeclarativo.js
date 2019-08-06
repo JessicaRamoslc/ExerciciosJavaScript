a4d63a1ab418a035a6ddb126e60fccdcbdd2bde9
748413 <Jessica.Colina@Cognizant.com>
2019-08-06 12:10:03 -0300

    Código imperativo X código Declarativo

---
+++ Array/imperativoVersusDeclarativo.js
@@ -0,0 +1,18 @@
+const alunos = [
+    { nome: 'Joana', nota: 7.9 }, 
+    { nome: 'Maria', nota: 9.2 }
+]
+
+// Imperativo
+let total1 = 0
+for(var i = 0; i < alunos.length; i++){
+    total1 += alunos[i].nota
+}
+
+console.log(total1 / alunos.length)
+
+// Declarativo: (Deixa a prorpia linguagem resolver)se importa menos em 'como' fazer. ou seja, nos detalhes internos, e se preocupa mais em O QUE fazer:
+const getNota = aluno => aluno.nota
+const soma = (total, atual) => total + atual
+const total2 = alunos.map(getNota).reduce(soma)
+console.log(total2 / alunos.length)
