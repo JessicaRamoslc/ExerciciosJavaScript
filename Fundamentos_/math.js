b991a071ab2d21f9b5bc8593f2bd69ebcfbfb971
JessLane <laneramos@gmail.com>
2019-08-19 23:30:32 -0300

    Fundamentos

--- Fundamentos_/math.js
+++
@@ -1,4 +0,0 @@
-const raio = 5.6 //declarando uma constante
-const area = Math.PI * Math.pow(raio, 2)//objeto Math, tem como constante o PI, e pow(função de Math) exige 2 parâmetro, o raio e a exponenciação
-
-console.log(area)
